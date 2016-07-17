1800a848b737d95a817175b9defe8f6bc8c6d13c
John <ajgrayson@gmail.com>
2016-07-17 20:58:25 +1200

    added routine support

--- controller/custom.js
+++ controller/custom.js
@@ -1,14 +1,83 @@
 var ipAddress = "192.168.4.1";
 
-function sendCommand(cmd) {
+function dooer() { }
+dooer.prototype.run = function() {
+  if (this.func){
+    this.func();
+  }
+}
+dooer.prototype.dothis = function(fnc, tim) {
+  fnc();
+  var d = new dooer();
+  setTimeout(function() {
+    d.run();
+  }, tim || 5000);
+  return d;
+}
+dooer.prototype.then = function(fnc, tim) {
+  var d = new dooer();
+  this.func = function() {
+    fnc();
+    setTimeout(function() {
+      d.run();
+    }, tim || 5000);
+  }
+  return d;
+}
+
+function checkConnection() {
   $.ajax({
-    url: "http://" + ipAddress+ "/action?cmd=" + cmd,
+    url: "http://" + ipAddress + "/action",
     success: function() {
-      enable();
+      $('#conn-status').removeClass('disconnected').addClass('connected').text('CONNECTED');
+      setTimeout(checkConnection, 30000);
     },
     error: function() {
-      enable();
+      $('#conn-status').removeClass('connected').addClass('disconnected').text('NOT CONNECTED');
+      setTimeout(checkConnection, 5000);
     }
+  })
+}
+
+checkConnection();
+
+function createLog(cmd) {
+  var en = $('<div class="entry pending">' + cmd + "</div>");
+  var child = $('#log').find('.entry').first();
+  if(child.length == 0) {
+    $('#log').append(en);
+  } else {
+    child.before(en);
+  }
+  return en;
+}
+
+function updateLog(en, success) {
+  if(success) {
+    en.removeClass('pending').addClass('success');
+  }else{
+    en.removeClass('pending').addClass('failed');
+  }
+}
+
+function sendCommand(cmd) {
+  var cmdup = cmd.toUpperCase();
+  var logEntry = createLog(cmdup);
+
+  $.ajax({
+    url: "http://" + ipAddress+ "/action?cmd=" + cmdup,
+    success: function() {
+      return function() {
+        updateLog(logEntry, true);
+        enable();
+      }
+    }(),
+    error: function() {
+      return function() {
+        updateLog(logEntry, false);
+        disable();
+      }
+    }()
   });
 }
 
@@ -19,16 +88,6 @@ function enable() {
 function disable() {
 
 }
-//
-// function left() {
-//   var val = $("#cmd-left").val();
-//   sendCommand("L" + val);
-// }
-//
-// function right() {
-//   var val = $("#cmd-right").val();
-//   sendCommand("R" + val);
-// }
 
 function left() {
     sendCommand('ML-150|MR150');
@@ -50,8 +109,18 @@ function update() {
   sendCommand("ML" + lt + "|MR" + rt);
 }
 
+function doRoutine(cmd) {
+  var d = new dooer();
+  d.dothis(forward).then(left, 10000).then(right);
+}
+
 function sendTextCommand() {
-  sendCommand($("#text-command").val());
+  var cmd = $("#text-command").val().toUpperCase();
+  if(cmd.indexOf('ROUTINE') == 0) {
+    doRoutine(cmd);
+  } else{
+    sendCommand(cmd);
+  }
   $("#text-command").val('');
   $("#text-command").focus();
 }
@@ -91,5 +160,7 @@ window.onload = function() {
     }
     rt = setTimeout(update, timeout);
   });
+
+  $('#execute-text-command').click(sendTextCommand);
 
 }
